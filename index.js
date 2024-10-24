9167427faf9b967cdea1c4569361fa439f1f7a41
kelvinempire <kelvinewurum@gmail.com>
2024-10-24 22:58:32 +0100

    update

--- index.js
+++ index.js
@@ -13,3 +13,37 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const texts = [
+    { el: document.getElementById("typing2"), text: "Super value deals" },
+    {
+      el: document.getElementById("typing4"),
+      text: "Save more every Friday on our Friday's shopping promo prize",
+    },
+  ];
+
+  texts.forEach((item, index) => {
+    item.el.textContent = ""; // Clear text content before typing
+    setTimeout(() => {
+      typeEffect(item.el, item.text);
+    }, index * 1000);
+  });
+
+  function typeEffect(element, text) {
+    let i = 0;
+    const speed = 50; // Adjust typing speed
+
+    function type() {
+      if (i < text.length) {
+        element.textContent += text.charAt(i);
+        i++;
+        setTimeout(type, speed);
+      }
+    }
+
+    type();
+  }
+});
+
